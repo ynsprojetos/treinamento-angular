3fb652a2b26a4c4dc0af453d7d4e4a7fbd221df5
Yuri Silva <ynscontato@gmail.com>
2022-05-30 21:38:05 -0300

    Primeiro commit

---
+++ primeiro-projeto/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+    <p> Meu primeiro componetn com Angular</p>
+  `
+})
+export class MeuPrimeiroComponent {
+
+}
